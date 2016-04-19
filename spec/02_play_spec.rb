c24500a46ed8e0fd73b2bedf741857ea95412264
Avidor Turkewitz <aviturkewitz@gmail.com>
2016-04-19 16:15:43 -0400

    Update test to 'play' all moves an expect board to be full.
    Test was hitting infinite loop in some cases if user checked to
    see if there were any empty spots to stop play loop. Previous
    test did not actually play any moves. New test plays all possible
    moves and checks that board looks as it should after that.

--- spec/02_play_spec.rb
+++ spec/02_play_spec.rb
@@ -4,8 +4,10 @@ describe '#play' do
   it 'calls turn nine times' do
     board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
-    expect(self).to receive(:turn).at_least(9).times
-
+    allow(self).to receive(:gets).and_return('1','2','3','4','5','6','7','8','9')
+    
     play(board)
+
+    expect(board).to eq(["X","X","X","X","X","X","X","X","X",])
   end
 end
