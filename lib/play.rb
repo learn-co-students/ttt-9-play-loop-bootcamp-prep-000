f597483c9cffdcb9efdd27b7776b7ae50ff625f3
Dequan Xiao <splscoupon@gmail.com>
2016-10-19 19:53:45 +0000

    Done.

--- lib/play.rb
+++ lib/play.rb
@@ -36,3 +36,15 @@ def turn(board)
 end
 
 # Define your play method below
+
+def play(board)
+  turn_count = 0
+  loop do
+    turn_count +=1
+    if turn_count <= 9
+    turn(board)
+    else
+      break
+    end
+  end
+end
