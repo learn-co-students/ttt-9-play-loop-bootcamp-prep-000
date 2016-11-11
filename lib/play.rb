cae3704f0ec582c6228fce21e04c060005fad65e
Wenna Jia <wennajia@gmail.com>
2016-11-11 06:36:50 +0000

    Done.

--- lib/play.rb
+++ lib/play.rb
@@ -40,10 +40,6 @@ def play(board)
   valid_turns = 0
   while valid_turns < 9
     turn(board)
-    if true
-      valid_turns += 1
-    else
-      valid_turns
-    end
+    valid_turns += 1
   end
 end
