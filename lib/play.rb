73b5cd3178c4298803e23cb4e203dcb0d511a75e
Charles Wooley <cwooley89@gmail.com>
2017-04-11 17:33:43 +0000

    Done.

--- lib/play.rb
+++ lib/play.rb
@@ -36,3 +36,45 @@ def turn(board)
 end
 
 # Define your play method below
+def has_won?(board)
+  won = false
+  row1 = [board[0], board[1], board[2]]
+  row2 = [board[3], board[4], board[5]]
+  row3 = [board[6], board[7], board[8]]
+  col1 = [board[0], board[3], board[6]]
+  col2 = [board[1], board[4], board[7]]
+  col3 = [board[2], board[5], board[8]]
+  dia1 = [board[0], board[4], board[8]]
+  dia2 = [board[2], board[4], board[6]]
+
+  rowCols = [row1, row2, row3, col1, col2, col3, dia1, dia2]
+
+  rowCols.each do |row|
+    if row[0] + row[1] + row[2] == "XXX" || row[0] + row[1] + row[2] == "OOO"
+      won = true
+    end
+  end
+  won
+end
+
+def moves_left?(board)
+  moves_left = false
+  board.each_with_index do |square, index|
+    if valid_move?(board, index)
+      moves_left = true
+    end
+  end
+  moves_left
+end
+
+def play(board)
+  #better way!
+  #until !moves_left?(board) || has_won?(board) do
+  #  turn(board)
+  #end
+  counter = 1
+  until counter == 10 do
+    turn(board)
+    counter += 1
+  end
+end
