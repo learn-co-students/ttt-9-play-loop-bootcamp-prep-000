e4da8ad8847288521344a44a5aab70b95afeb332
isaac LEVI <isaaclevi1710@gmail.com>
2017-04-25 14:47:55 +0000

    Done.

--- spec/02_play_spec.rb
+++ spec/02_play_spec.rb
@@ -5,9 +5,9 @@ describe '#play' do
     board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
     allow(self).to receive(:gets).and_return('1','2','3','4','5','6','7','8','9')
-    
+
     play(board)
 
-    expect(board).to eq(["X","X","X","X","X","X","X","X","X",])
+    expect(board).to eq(["X","X","X","X","X","X","X","X","X"])
   end
 end
