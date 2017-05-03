06d22db91c7ab7b15e0c8562801a3fd92f7a8eff
Suhan Wijaya <suhanw@gmail.com>
2017-05-03 15:36:01 +0000

    Done.

--- lib/play.rb
+++ lib/play.rb
@@ -23,16 +23,29 @@ def valid_move?(board, index)
   index.between?(0,8) && !position_taken?(board, index)
 end
 
-def turn(board)
+def turn(board, current_player)
   puts "Please enter 1-9:"
   input = gets.strip
   index = input_to_index(input)
   if valid_move?(board, index)
-    move(board, index)
+    move(board, index, current_player)
     display_board(board)
   else
-    turn(board)
+    turn(board, current_player)
   end
 end
 
 # Define your play method below
+def play(board)
+  current_turn = 0
+  current_player = "X"
+  while current_turn < 9
+    current_turn += 1
+    turn(board, current_player)
+    #if current_player == "X"
+    #  current_player = "O"
+    #else
+    #  current_player = "X"
+    #end
+  end
+end
