e675952c3dab7d327f2b233f0ff18335477ab812
Rob Baranowski <robbaranowski@gmail.com>
2017-07-24 19:00:47 +0000

    Done.

--- lib/play.rb
+++ lib/play.rb
@@ -23,16 +23,43 @@ def valid_move?(board, index)
   index.between?(0,8) && !position_taken?(board, index)
 end
 
-def turn(board)
+def turn(board, player="X")
   puts "Please enter 1-9:"
   input = gets.strip
   index = input_to_index(input)
   if valid_move?(board, index)
-    move(board, index)
+    move(board, index, player)
     display_board(board)
   else
-    turn(board)
+    turn(board, player)
   end
 end
 
+GAME_OVER = false
 # Define your play method below
+def play(board)
+  numturns = 0
+  player = "X"
+  until numturns == 9
+    turn(board, player)
+    player = switch_player(player)
+    numturns += 1
+  end
+end
+
+def switch_player(player)
+  if player == "X"
+    return "O"
+  elsif player == "O"
+    return "X"
+  end
+end
+
+=begin
+def win?(board)
+#returns true if there are three in a row somewhere in board, false otherwise
+  #there are 3 rows, 3 cols, and 3 diags to check
+
+  GAME_OVER = true
+end
+=end
