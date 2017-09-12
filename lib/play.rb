8c5141e771e95d81161f6da8f8b9b43cb5f15259
Iain Olliver <iain.olliver@gmail.com>
2017-09-12 13:35:31 +0000

    Done.

--- lib/play.rb
+++ lib/play.rb
@@ -36,3 +36,11 @@ def turn(board)
 end
 
 # Define your play method below
+def play(board)
+  turns = 0
+  game_over = false
+  while !game_over && turns < 9
+    turn(board)
+    turns += 1
+  end
+end
