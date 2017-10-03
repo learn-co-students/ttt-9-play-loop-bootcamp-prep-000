ce2c16eb1029df9473e1f256772eeeb39f2ee1b5
Adam Clayman <adam@clayman.org>
2017-10-03 00:05:49 +0000

    Done.

--- lib/play.rb
+++ lib/play.rb
@@ -36,3 +36,14 @@ def turn(board)
 end
 
 # Define your play method below
+def play(board)
+  turn(board)
+  turn(board)
+  turn(board)
+  turn(board)
+  turn(board)
+  turn(board)
+  turn(board)
+  turn(board)
+  turn(board)
+end
