93dd9b514c78d7bdbb969e1869cd8c09395c79b6
Cynthia Maldonado <cmaldonado416@gmail.com>
2017-10-20 00:25:59 +0000

    Done.

--- lib/play.rb
+++ lib/play.rb
@@ -36,3 +36,17 @@ def turn(board)
 end
 
 # Define your play method below
+
+
+def play(board)
+  9.times do
+    turn(board)
+  end
+end
+
+#def play(board)
+#turn_count = 0
+#until turn_count == 9
+  #turn_count += 1
+#end
+#end
