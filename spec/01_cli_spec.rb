20399733bcab511d34fbc5a0ef7dc486655ba25d
STELLA PONG KEXIN <stellapong0513@hotmail.com>
2017-11-04 12:51:36 +0000

    Done.

--- spec/01_cli_spec.rb
+++
@@ -1,9 +0,0 @@
-describe './bin/play' do
-  it 'evokes #play passing it `board` as an argument' do
-    allow($stdout).to receive(:puts)
-
-    expect(self).to receive(:play).with(kind_of(Array))
-
-    run_file("./bin/play")
-  end
-end
