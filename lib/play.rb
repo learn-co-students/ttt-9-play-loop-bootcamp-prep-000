7dfdfa19a55d50f799b8299dfd1ab556f50abd0d
michael lynn <michael_lynn@comcast.net>
2018-01-01 07:36:32 +0000

    Done.

--- lib/play.rb
+++ lib/play.rb
@@ -1,38 +1,62 @@
-# Helper Methods
+# LIB/turn.rb files begin here:
+#_________________________________________________
+
+def input_to_index(input)
+  input.to_i - 1
+end 
+
+#if index is valid...
+def valid_move?(number_entered, board)
+  number_entered.between?(0, 8) && !(position_taken?(board, number_entered))
+end
+#if index is valid...make the move for index
+def move(array, index, name = "X")
+  array[index] = name
+end
+#if index is valid...show the board
+def position_taken?(board, answer)
+  board[answer] != " " 
+end
+
 def display_board(board)
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
-end
-
-def input_to_index(user_input)
-  user_input.to_i - 1
-end
-
-def move(board, index, current_player = "X")
-  board[index] = current_player
-end
-
-def position_taken?(board, location)
-  board[location] != " " && board[location] != ""
-end
-
-def valid_move?(board, index)
-  index.between?(0,8) && !position_taken?(board, index)
+   puts " #{board[0]} | #{board[1]} | #{board[2]} "
+   puts "-----------"
+   puts " #{board[3]} | #{board[4]} | #{board[5]} "
+   puts "-----------"
+   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
 def turn(board)
-  puts "Please enter 1-9:"
-  input = gets.strip
-  index = input_to_index(input)
-  if valid_move?(board, index)
-    move(board, index)
-    display_board(board)
-  else
+    puts "Please enter 1-9:"
+    answer = gets.chomp
+    answer = input_to_index(answer)
+    if valid_move?(answer, board)
+      move(board, answer)
+      display_board(board)
+      #turn(board)
+    else
+      puts "That is an invalid entry!"
+      turn(board)
+    end 
+end
+
+def play(board)
+  turns = 3
+  counter = 0
+  loop do
+    counter = counter + 1
     turn(board)
+    if counter >= turns
+      puts "You only get #{turns} turns baby!"
+    end
   end
 end
 
-# Define your play method below
+#else ask for input again until you get valid input
+#_________________________________________________
+
+board = ["X"," "," "," "," "," "," "," "," "]
+puts "Welcome to Tic Tac Toe!"
+puts display_board(board)
+#turn(board)
+play(board)
