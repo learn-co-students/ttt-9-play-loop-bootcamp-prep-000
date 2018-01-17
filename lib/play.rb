da3daa866fd525db711e5d7f639d646c94955b16
jencekade <gaephuoc@gmail.com>
2018-01-17 08:41:43 +0000

    Done.

--- lib/play.rb
+++ lib/play.rb
@@ -37,15 +37,9 @@ end
 
 # Define your play method below
 def play(board)
-  9.times do
-    turn(board)
-  end
-end
-
-=begin
   counter = 0
   while counter < 9
     turn(board)
     counter += 1
   end
-=end
+end
