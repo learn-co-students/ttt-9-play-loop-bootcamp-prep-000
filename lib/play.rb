927a42e92711ce19788792f6e6648768e9f176b1
Andrew Trent <andrewtrentdesign@gmail.com>
2018-02-06 18:39:18 +0000

    Done.

--- lib/play.rb
+++ lib/play.rb
@@ -1,3 +1,5 @@
+
+
 # Helper Methods
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
@@ -36,3 +38,11 @@ def turn(board)
 end
 
 # Define your play method below
+
+def play(board)
+  counter = 0
+  until counter == 9
+    turn(board)
+    counter += 1
+  end
+end
