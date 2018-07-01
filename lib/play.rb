2c8525e6def80341dcaf802c431181d675cd1e78
gmat191@gmail.com <gmat191@gmail.com>
2018-07-01 00:56:04 +0000

    Done.

--- lib/play.rb
+++ lib/play.rb
@@ -37,9 +37,9 @@ end
 
 # Define your play method below
 def play(board)
-  counter = 0
-  until counter == 9   
-    turn(board)
-    counter += 1 
+  counter = 0 
+  until counter == 9 
+  turn(board)
+  counter += 1 
   end
 end
