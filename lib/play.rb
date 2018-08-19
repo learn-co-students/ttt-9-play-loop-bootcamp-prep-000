3a604423867b036a3778f68d888f577c1f34de11
Michael Pieschel <sixtyhectares@yahoo.com>
2018-08-19 23:19:18 +0000

    Done.

--- lib/play.rb
+++ lib/play.rb
@@ -34,5 +34,10 @@ def turn(board)
     turn(board)
   end
 end
-
-# Define your play method below
+def play(board)
+   turns = 0
+        until turns == 9
+        turn(board)
+        turns += 1
+      end
+end
