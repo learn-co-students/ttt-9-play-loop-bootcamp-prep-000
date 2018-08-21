baa1f2d864152dd09f0d45d41b1f22ad84297cb0
John Werenski <jweredc@gmail.com>
2018-08-21 20:47:12 +0000

    Done.

--- lib/play.rb
+++ lib/play.rb
@@ -31,8 +31,16 @@ def turn(board)
     move(board, index)
     display_board(board)
   else
+    puts "space is occupied or invalid entry, try again."
     turn(board)
   end
 end
 
 # Define your play method below
+def play(board)
+  counter = 0 
+    while counter < 9 
+    turn(board)
+    counter += 1
+  end 
+end 
