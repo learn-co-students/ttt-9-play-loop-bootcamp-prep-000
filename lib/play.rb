c9bb567affc4e45a2cf9ecc3cc29299c5e01f1de
beneficial-bundle-8122 <satyasinha94@gmail.com>
2018-09-30 22:24:15 +0000

    Done.

--- lib/play.rb
+++ lib/play.rb
@@ -35,4 +35,10 @@ def turn(board)
   end
 end
 
-# Define your play method below
+def play(board)
+  turncount = 0
+  until turncount == board.length
+    turn(board)
+    turncount += 1
+  end
+end
