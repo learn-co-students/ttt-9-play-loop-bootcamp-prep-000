26c8591cd5f0e143a835800811a149bfb18d4c0c
Mark Kauffman <markkauffman22@gmail.com>
2019-10-09 20:31:30 +0000

    Done.

--- lib/play.rb
+++ lib/play.rb
@@ -11,16 +11,16 @@ def input_to_index(user_input)
   user_input.to_i - 1
 end
 
-def move(board, index, current_player = "X")
-  board[index] = current_player
-end
-
 def position_taken?(board, location)
   board[location] != " " && board[location] != ""
 end
 
 def valid_move?(board, index)
   index.between?(0,8) && !position_taken?(board, index)
+end
+
+def move(board, index, current_player = "X")
+  board[index] = current_player
 end
 
 def turn(board)
@@ -30,9 +30,26 @@ def turn(board)
   if valid_move?(board, index)
     move(board, index)
     display_board(board)
+    return index
   else
     turn(board)
   end
 end
 
 # Define your play method below
+
+def play(board)
+  turns = [1,2,3,4,5,6,7,8,9]
+  
+  while turns.length > 0
+    spent_index = turn(board)
+    spent_input = spent_index + 1
+    
+    # remove the spent_input
+    turns.delete(spent_input)
+  end
+end  
+  
+  
+
+
